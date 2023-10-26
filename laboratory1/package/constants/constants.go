02b92cfad33c3be80132c888e750927fad5658b4
Недорезов Данил <nedorezov.dd@edu.spbstu.ru>
2023-10-26 22:13:17 +0300

    add all

---
+++ laboratory1/package/constants/constants.go
@@ -0,0 +1,9 @@
+package constants
+
+const plus string = "+"
+const minus string = "-"
+const devide string = "/"
+const multiply string = "*"
+
+const devisionByZero string = "Ошибка: деление на ноль невозможно"
+const incorrectOperation = "Некорректное операция"
